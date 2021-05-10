f1d3400ff84795ca49306d42b014e9ba6506c4bb
ashariwdd2 <73391054+ashariwdd2@users.noreply.github.com>
2021-05-11 02:15:24 +0700

    Update DetailDinas.java

--- app/src/main/java/com/example/recycleview173/DetailDinas.java
+++ app/src/main/java/com/example/recycleview173/DetailDinas.java
@@ -1,4 +1,34 @@
 package com.example.recycleview173;
 
-public class DetailDinas {
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+
+public class DetailDinas extends AppCompatActivity {
+    ImageView dtlogo;
+    TextView dtnama, dtdeskripsi;
+
+    String namadinas;
+    String desdinas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_dinas);
+
+        dtlogo = findViewById(R.id.logo_detail);
+        dtnama = findViewById(R.id.nama_detail);
+        dtdeskripsi = findViewById(R.id.deskripsi_detail);
+
+        namadinas = getIntent().getStringExtra("namadinas");
+        desdinas = getIntent().getStringExtra("desdinas");
+
+        dtnama.setText(namadinas);
+        dtdeskripsi.setText(desdinas);
+        Glide.with(this).load(getIntent().getIntExtra("logodinas", 0)).into(dtlogo);
+    }
 }
